f4bb46a852e333f37f39e9535816a22ebd3c2a01
mosheco2 <mosheco2@gmail.com>
2026-04-23 11:51:10 +0300

    Update business-app.js

--- public/business-app.js
+++ public/business-app.js
@@ -8097,10 +8097,6 @@ window.getAnalyticsAIInsight = async function() {
     });
 };
 
-    if (subTab === 'analytics') {
-        setTimeout(() => { if (typeof window.renderAnalytics === 'function') window.renderAnalytics(); }, 150);
-    }
-
 setInterval(() => {
     const tabSales = getEl('tab-sales');
     if (tabSales && tabSales.classList.contains('tab-active')) {
