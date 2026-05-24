74675413bbe6d09703ee17f136f2e29c9b04df88
mosheco2 <mosheco2@gmail.com>
2026-05-24 20:17:37 +0300

    Update test-db.js

--- test-db.js
+++ test-db.js
@@ -2,13 +2,18 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
     ssl: { rejectUnauthorized: false }
 });
 
 pool.connect()
-    .then(() => {
-        console.log("✅ חיבור הצליח! מסד הנתונים נגיש.");
+    .then((client) => {
+        console.log("✅ חיבור הצליח! מסד הנתונים נגיש לחלוטין מ-Render.");
+        client.release();
         process.exit();
     })
     .catch(err => {
